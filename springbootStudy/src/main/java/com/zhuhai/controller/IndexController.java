b4b050f385d2af825561904ee34c69fe2e2a0b33
zhuhai <hpuzhuhai@gmail.com>
2019-03-19 23:45:18 +0800

    添加logback日志和Thymeleaf

--- springbootStudy/src/main/java/com/zhuhai/controller/IndexController.java
+++ springbootStudy/src/main/java/com/zhuhai/controller/IndexController.java
@@ -1,10 +1,16 @@
 package com.zhuhai.controller;
 
 import com.zhuhai.config.MyConfig2;
+import com.zhuhai.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA
@@ -14,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: hai
  */
 @Slf4j
-@RestController
+@Controller
 public class IndexController {
 
     /*@Autowired
@@ -24,13 +30,57 @@ public class IndexController {
     private MyConfig2 myConfig2;
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
 
         /*log.info("my name is {}", myConfig.getName());
         log.info("my pwd is {}", myConfig.getPwd());*/
         log.info("my name is {}", myConfig2.getName());
         log.info("my pwd is {}", myConfig2.getPwd());
+        log.warn("warn log");
+        log.error("error log");
+        model.addAttribute("name", myConfig2.getName());
+        return "index";
+    }
 
-        return "hello springBoot!";
+    @GetMapping(value = "/user/info")
+    public String getUser(Model model) {
+        User user = new User();
+        user.setUserName("张三");
+        user.setAge(26);
+        user.setSex("男");
+        user.setEmail("123@qq.com");
+        model.addAttribute("user", user);
+        return "index";
+    }
+
+    @GetMapping(value = "/user/list")
+    public String listUser(Model model) {
+        User user = new User();
+        user.setUserName("张三");
+        user.setAge(26);
+        user.setSex("男");
+        user.setEmail("123@qq.com");
+
+        User user2 = new User();
+        user2.setUserName("李四");
+        user2.setAge(24);
+        user2.setSex("男");
+        user2.setEmail("lisi@qq.com");
+
+        User user3 = new User();
+        user3.setUserName("小仙女");
+        user3.setAge(18);
+        user3.setSex("女");
+        user3.setEmail("test@163.com");
+
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+        userList.add(user2);
+        userList.add(user3);
+
+        model.addAttribute("userList", userList);
+        return "index";
+
+
     }
 }
---
+++ springbootStudy/src/main/java/com/zhuhai/entity/User.java
@@ -0,0 +1,21 @@
+package com.zhuhai.entity;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA
+ * Date: 2019/3/19
+ * Time: 22:59
+ *
+ * @author: hai
+ */
+@Data
+public class User {
+
+    private String userName;
+    private Integer age;
+    private String sex;
+    private String email;
+
+
+}
