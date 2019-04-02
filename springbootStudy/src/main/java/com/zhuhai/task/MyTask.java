8d6914f4ca1424b03ac280cfc37ea7f39c86b203
ChinaSea <hpuzhuhai@163.com>
2019-04-02 19:04:26 +0800

    定时任务

--- springbootStudy/src/main/java/com/zhuhai/task/MyTask.java
+++ springbootStudy/src/main/java/com/zhuhai/task/MyTask.java
@@ -1,7 +1,6 @@
 package com.zhuhai.task;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -20,20 +19,23 @@ public class MyTask {
     //@Async
     //@Scheduled(cron = "0/5 * * * * ?")
     public void task1() throws InterruptedException {
-        //Thread.sleep(2000);
         log.info("task execute：{}", LocalDateTime.now());
+        Thread.sleep(2000);
+
     }
 
     //@Scheduled(fixedRate = 1000)
     public void task2() throws InterruptedException {
-        Thread.sleep(2000);
         log.info("task2 execute...");
+        Thread.sleep(2000);
+
     }
 
-    @Scheduled(fixedDelay = 3000)
+    //@Scheduled(fixedDelay = 3000)
     public void task3() throws InterruptedException {
-        Thread.sleep(2000);
         log.info("task2 excute...");
+        Thread.sleep(2000);
+
     }
 
 
